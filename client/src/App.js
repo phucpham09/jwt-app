6af6e602ea012c2a36e6120a6f39c0784b81de96
phucpham <phamhuuphuc4049@gmail.com>
2024-08-06 15:07:19 +0700

    up github

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Login, SignUp } from "./pages";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
